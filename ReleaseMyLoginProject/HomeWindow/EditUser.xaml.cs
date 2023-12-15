6c7c5aaa579baa718b2b96104b4222b83a1565c5
nohodoevs <nohodoevs@311-1>
2023-12-15 11:35:32 +0800

    Окно для Редактирования Username

--- ReleaseMyLoginProject/HomeWindow/EditUser.xaml.cs
+++ ReleaseMyLoginProject/HomeWindow/EditUser.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ReleaseMyLoginProject.Pages.PageAdmin
+namespace ReleaseMyLoginProject.HomeWindow
 {
     /// <summary>
-    /// Логика взаимодействия для PageEditUser.xaml
+    /// Логика взаимодействия для EditUser.xaml
     /// </summary>
-    public partial class PageEditUser : Page
+    public partial class EditUser : Window
     {
-        public PageEditUser()
+        public EditUser()
         {
             InitializeComponent();
         }
--- ReleaseMyLoginProject/Pages/PageAdmin/PageEdit.xaml.cs
+++ ReleaseMyLoginProject/Pages/PageAdmin/PageEdit.xaml.cs
@@ -50,5 +50,9 @@ namespace ReleaseMyLoginProject.Pages.PageAdmin
         {
 
         }
+        
+
+
+
     }
 }
